fa2dd5fe7b2f84e14e6427df2a7a9ade60d68c20
srinivasulu2004 <srinivasulubehara@gmail.com>
2025-08-01 14:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
     <html>
     <head><title>My Flask EC2 App</title></head>
     <body style="font-family: Arial; margin: 40px;">
-        <h1>Welcome to Flask App on EC2!</h1>
-        <p>This app is running directly on EC2 without Docker.</p>
+        <p>hi guys....................</p>
+        <h1>IT'S FROM "dev" ENVIRONMENT </h1>
         <ul>
             <li><a href="/fetch">/fetch - External API Call</a></li>
             <li><a href="/health">/health - Health Check</a></li>
